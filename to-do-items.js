578404e224241bb5009533d04820ca081fc13d63
Sammani Ariyarathne <sammani.ari@gmail.com>
2023-04-10 00:33:04 +0200

    rendering todo items from API

--- to-do-items.js
+++ to-do-items.js
@@ -1,15 +1,28 @@
 /** @format */
+const todoListEl =
+	document.getElementById("myUL");
+const todoItemEl =
+	document.getElementById("anItem");
 
 async function fetchingData() {
 	const res = await fetch(
-		"http://127.0.0.1:3000/",
-		{
-			mode: "no-cors",
-		}
+		"http://127.0.0.1:3000/"
+		/*{
+			headers: {
+				"Content-Type":
+					"application/json",
+			},
+		}*/
 	);
 	const data = await res.json();
 	console.log(data);
+
+	for (let element of data) {
+		console.log(element);
+		const newItem =
+			document.createElement("li");
+		newItem.innerHTML = element.todo;
+		todoListEl.append(newItem);
+	}
 }
 fetchingData();
-const testingApi =
-	document.getElementById("sam");
